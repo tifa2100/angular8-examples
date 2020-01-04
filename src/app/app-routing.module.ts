37bc629b4468388e57320dc41bcdf2a364809856
Mostafa Bayomi <tifa2100@gmail.com>
2020-01-04 13:23:05 +0300

    Material Table with Checkbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialTbComponent } from './datatables/material-tb/material-tb.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MaterialTbComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatTableModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material';
+import { MatRadioModule } from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialTbComponent } from './datatables/material-tb/material-tb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialTbComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
