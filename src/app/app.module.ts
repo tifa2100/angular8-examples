55e2858381cd2a635af5896e7ccf2739ccf4ca7d
Mostafa Bayomi <tifa2100@gmail.com>
2020-01-04 16:16:34 +0300

    Add Mat Angular Full Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MatTableModule } from '@angular/material';
 import { MatCheckboxModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material';
@@ -9,15 +14,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialTbComponent } from './datatables/material-tb/material-tb.component';
 import { MatTbRadiobtnComponent } from './datatables/mat-tb-radiobtn/mat-tb-radiobtn.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MaterialTbComponent,
     MatTbRadiobtnComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+
     AppRoutingModule,
     MatTableModule,
     MatCheckboxModule,
