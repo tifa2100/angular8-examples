6723baa7ca984f367e8eee8acd80d0c3aed470a8
Mostafa Bayomi <tifa2100@gmail.com>
2020-01-04 16:50:53 +0300

    Add Mat Table bind to datasource

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MaterialTbComponent } from './datatables/material-tb/material-tb.component';
 import { MatTbRadiobtnComponent } from './datatables/mat-tb-radiobtn/mat-tb-radiobtn.component';
+import { MatFullFeatureComponent } from './datatables/mat-full-feature/mat-full-feature.component';
 
 
 const routes: Routes = [
   { path: '', component: MaterialTbComponent},
   { path: 'radio', component: MatTbRadiobtnComponent},
+  { path: 'featured', component: MatFullFeatureComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { MatTableModule } from '@angular/material';
 import { MatCheckboxModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material';
 
@@ -14,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialTbComponent } from './datatables/material-tb/material-tb.component';
 import { MatTbRadiobtnComponent } from './datatables/mat-tb-radiobtn/mat-tb-radiobtn.component';
+import { MatFullFeatureComponent } from './datatables/mat-full-feature/mat-full-feature.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { LayoutComponent } from './layout/layout/layout.component';
@@ -23,6 +23,7 @@ import { LayoutComponent } from './layout/layout/layout.component';
     AppComponent,
     MaterialTbComponent,
     MatTbRadiobtnComponent,
+    MatFullFeatureComponent,
     HeaderComponent,
     SidenavListComponent,
     LayoutComponent,
@@ -34,7 +35,6 @@ import { LayoutComponent } from './layout/layout/layout.component';
     FlexLayoutModule,
 
     AppRoutingModule,
-    MatTableModule,
     MatCheckboxModule,
     MatRadioModule
   ],
